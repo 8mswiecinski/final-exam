6bfc67cc9a667e3dc32f083c2bd1b74e2d332602
8mswiecinski <8mswiecinski@stpatrick.org>
2016-12-13 10:52:06 -0600

    day2

---
+++ item.js
@@ -0,0 +1,15 @@
+class Item{
+  //Fill in the item class below this comment.
+constructor(n,p,st){
+  this.name = n;
+  this.price = p;
+this.shippingTime = st;
+}
+
+//Create your three test items below this comment.
+}
+
+
+let mygift = new Item("Ps4", 400, 10);
+let yourGift = new Item("Macbook pro",1,500, 20);
+let hisgift = new item("ipad",500,6);
